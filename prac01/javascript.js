6a96e1bd788e743666389a89642d1ec5ff94350d
juyeonyy <juyeony.jyy@gmail.com>
2023-03-13 12:44:41 +0900

    javascript practice 01

---
+++ prac01/javascript.js
@@ -0,0 +1,15 @@
+console.log(23 + 97);
+console.log(2 + 3 + 4 + 5 + 6 + 7);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b);
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
